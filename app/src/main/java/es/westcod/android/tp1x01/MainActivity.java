b6182575d9a62c5a5fc6cede65415cbce0a5d0cd
ampedPF-LT <ampedpf@gmail.com>
2015-03-26 21:15:19 +0100

    covered drawing with empty fields

--- app/src/main/java/es/westcod/android/tp1x01/MainActivity.java
+++ app/src/main/java/es/westcod/android/tp1x01/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -16,6 +18,7 @@ public class MainActivity extends ActionBarActivity {
     public EditText editText_Y;
     public EditText editText_Width;
     public EditText editText_Height;
+    public ArrayList<EditText> textFields;
     public GeometryLayer geom;
 
     @Override
@@ -27,6 +30,7 @@ public class MainActivity extends ActionBarActivity {
         editText_Y = (EditText) findViewById(R.id.editText_Y);
         editText_Width = (EditText) findViewById(R.id.editText_Width);
         editText_Height = (EditText) findViewById(R.id.editText_Height);
+        textFields = new ArrayList<>();
         geom = (GeometryLayer) findViewById(R.id.drawing_layer);
     }
 
@@ -54,19 +58,10 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void resetFields(){
-        editText_X.setText("0");
-        editText_Y.setText("0");
-        editText_Width.setText("0");
-        editText_Height.setText("0");
-
-        global_layout.requestFocus();
-    }
-
-    public void refreshFields(){
-        editText_X.setText(Integer.toString(Integer.parseInt(editText_X.getText().toString())));
-        editText_Y.setText(Integer.toString(Integer.parseInt(editText_Y.getText().toString())));
-        editText_Width.setText(Integer.toString(Integer.parseInt(editText_Width.getText().toString())));
-        editText_Height.setText(Integer.toString(Integer.parseInt(editText_Height.getText().toString())));
+        editText_X.setText("");
+        editText_Y.setText("");
+        editText_Width.setText("");
+        editText_Height.setText("");
 
         global_layout.requestFocus();
     }
@@ -79,9 +74,31 @@ public class MainActivity extends ActionBarActivity {
         geom.clearArray();
     }
 
+    public boolean checkTextFields(){
+        boolean formIsInvalid = false;
+        if(textFields.size()<4){
+            textFields.clear();
+            textFields.add(editText_X);
+            textFields.add(editText_Y);
+            textFields.add(editText_Width);
+            textFields.add(editText_Height);
+        }
+        for (EditText et : textFields){
+            if(!et.getText().toString().isEmpty()){
+                et.setText(Integer.toString(Integer.parseInt(et.getText().toString())));
+            } else {
+                et.setText("0");
+                formIsInvalid = true;
+            }
+        }
+        global_layout.requestFocus();
+        return formIsInvalid;
+    }
+
     public void button_Draw_onClick(View button){
+        boolean formIsInvalid = checkTextFields();
         addRectToCanvas(Integer.parseInt(editText_X.getText().toString()), Integer.parseInt(editText_Y.getText().toString()), Integer.parseInt(editText_Width.getText().toString()), Integer.parseInt(editText_Height.getText().toString()));
-        refreshFields();
+        if(formIsInvalid){ resetFields();}
     }
 
     public void button_Clear_onClick(View button){
